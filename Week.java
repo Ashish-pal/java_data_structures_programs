d8bc42ad23577316b78eb3da6ffb48ffdd73a3c0
Ashish Pal <ak987982@gmail.com>
2021-11-16 17:09:41 +0530

    DSCalender.java week, weekday, calendaerqueue

---
+++ Week.java
@@ -0,0 +1,11 @@
+package dsCalendar;
+
+
+public class Week {
+	public MyQueue<WeekDay> weekDayQue;
+	
+	public Week(){
+		weekDayQue = new MyQueue<WeekDay>();
+	}
+	
+}
---
+++ WeekDay.java
@@ -0,0 +1,12 @@
+package dsCalendar;
+
+
+public class WeekDay{
+	public String date;
+	String day;
+	
+	public WeekDay(String day, String date){
+		this.day = day;
+		this.date = date;		
+	}
+}
